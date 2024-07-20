f4996ef7f96ddda48294af213623c845754874d8
Santhosh Kumar <santhosh10378@gmail.com>
2024-07-20 20:20:56 +0530

    ship genres with the app

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,7 +5,6 @@ import {
   Image,
   List,
   ListItem,
-  Spinner,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -16,10 +15,7 @@ interface Props {
 }
 
 const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
-  const { data: genres, loading, error } = useGenres();
-
-  if (error) return null;
-  if (loading) return <Spinner />;
+  const { data: genres } = useGenres();
 
   return (
     <>
