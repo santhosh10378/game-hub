6390da080a3587c6ca688ba2ac3cf1500677303b
Santhosh Kumar <santhosh10378@gmail.com>
2024-07-20 20:33:04 +0530

    Refactor useGames

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -11,8 +11,8 @@ import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 
 import { HStack, Icon } from "@chakra-ui/react";
-import { Platform } from "../hooks/useGames";
 import { IconType } from "react-icons";
+import { Platform } from "../hooks/usePlatforms";
 
 interface PlatformIconListProps {
   platforms: Platform[];
