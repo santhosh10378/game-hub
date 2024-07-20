2b1df264a69b378f15f325b0ce45bfeaef6710ce
Santhosh Kumar <santhosh10378@gmail.com>
2024-07-21 05:17:15 +0530

    Separated API Key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -8,6 +8,6 @@ export interface FetchResponse<T> {
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "f49d2b1dcc414ba8afb090756f92f6fc",
+    key: import.meta.env.VITE_GAME_HUB_API,
   },
 });
